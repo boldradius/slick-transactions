91b08c04da9c24e3ee8c9481bf9863b9244dc689
Patrick Premont <patrick.premont@boldradius.com>
2015-06-23 17:37:50 -0400

    Updated to take a DBIO for Slick 3

--- src/main/scala/com/boldradius/slick/transactions/package.scala
+++ src/main/scala/com/boldradius/slick/transactions/package.scala
@@ -2,12 +2,21 @@ package com.boldradius.slick
 
 import java.sql.{SQLException, Connection}
 
+import slick.dbio.DBIO
+
 import scala.annotation.tailrec
-import scala.slick.jdbc.JdbcBackend
-import scala.slick.driver.PostgresDriver
+import slick.jdbc.JdbcBackend
+import slick.driver.PostgresDriver
+import slick.driver.PostgresDriver.api._
+import slick.driver.PostgresDriver.JdbcActionExtensionMethods
+import slick.driver._
+import scala.concurrent.Await
 import scalaz.effect.IO
-import scala.slick.backend.DatabaseComponent
-import scala.slick.profile.BasicProfile
+import slick.backend.DatabaseComponent
+import slick.profile.BasicProfile
+import slick.driver.JdbcActionComponent
+import slick.jdbc.TransactionIsolation._
+import scala.concurrent.duration._
 
 /**
  * Created by ppremont on 2014-11-03.
@@ -15,7 +24,7 @@ import scala.slick.profile.BasicProfile
 package object transactions {
   /**
    * All database statements and transcations in the applicaction must be performed through
-   * [[transaction]]. This method records the function to be executed as a transaction,
+   * [[transaction]]. This method runs a [[DBIO]] transactionally
    * and wraps it in [[IO]] to prevent accidental nesting of transactions. The transaction will only
    * run when [[IO]] runs, through a call to [[IO.unsafePerformIO]]. This call should only occur near the
    * top level of the application (such as in a controller), where the calling context is clear and
@@ -24,28 +33,18 @@ package object transactions {
    * To avoid accidentally nested transactions, calls of [[IO.unsafePerformIO]] should definitely not occur
    * within utility functions. Functions that work on the database may instead
    *  a. return a Query that may then be composed into larger queries,
-   *  b. or take in the session, so that they can be run as part of an eventual transaction
+   *  b. return a DBIO so that they can be run as part of an eventual transaction
    *  c. or, as a last resort, return IO after having made one or more calls to [[transaction]].
    *
    * The transaction is set to use the serializable isolation level, and will retry immediately as many times
    * as necessary in the even of a serialization failure (when concurrent transactions have
    * invalidated the work of this transaction). No exponential back-off is currently implemented.
-   *
-   * Care must be taken not to leak the session out of the call to transaction. The returned type A should
-   * not contain the session: it will be closed and unusable. This can happen accidentally if you create a closure
-   * that closes over the session. In the presence of nested transactions, a call to [[IO.map]] on the inner transaction
-   * would have the outer session in lexical scope but its execution would be delayed until after the session is closed.
-   * We may solve this in the future by no longer exposing the session and using a monad to compose parts of the
-   * transaction.
    */
-  def transaction[A](db : PostgresDriver.backend.DatabaseDef)(txn: PostgresDriver.backend.Session => A): IO[A] = {
+  def transaction[A](db : PostgresDriver#Backend#DatabaseDef)(txn: DBIO[A]): IO[A] = {
     val serializationFailureErrorCode = "40001" // http://www.postgresql.org/docs/8.3/static/errcodes-appendix.html
     @tailrec def retryOnSerializationFailure: A =
       (try {
-        db.withTransaction { s =>
-          s.conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE)
-          Some(txn(s))
-        }
+        Some(Await.result(db.run(txn.withTransactionIsolation(Serializable)), Duration.Inf))
       } catch {
         case e:SQLException =>
           if (e.getSQLState() == serializationFailureErrorCode) None
